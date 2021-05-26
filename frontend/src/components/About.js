cd0720b3275d18e8c72abf82bf92717e1ae2e975
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2021-05-26 10:01:17 +0200

    add some change on main page -frontend

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <Container>
+      <Jumbotron>
+        <h1>O nas!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          tincidunt condimentum mi vel faucibus. Vivamus sed pretium ex, eget
+          cursus mi. Integer accumsan tortor dolor, non fermentum libero
+          consectetur vel. Aliquam erat nibh, volutpat at sodales id, congue a
+          ante. Sed fermentum sapien risus, eu viverra diam elementum convallis.
+          Suspendisse sagittis libero ut velit pretium placerat. Donec quis
+          ullamcorper ante, at ullamcorper libero. Mauris convallis justo felis,
+          at finibus velit iaculis vitae. Duis pellentesque mi nec dui cursus
+          imperdiet. Aenean congue orci ac leo ultricies scelerisque. Fusce ut
+          augue accumsan mi aliquet rutrum sit amet vel risus.
+        </p>
+      </Jumbotron>
+    </Container>
+  );
 };
 
 export default About;
